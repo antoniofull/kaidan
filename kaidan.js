6963165544b305342d5a3fd3f07a9db80befb6ce
Antonio Fullone <antoniof@gib-localadmins-iMac.local>
2015-06-17 15:03:56 +0200

    fixed issue with empty lines when splitting the url list

--- kaidan.js
+++ kaidan.js
@@ -1,6 +1,5 @@
 /**
- * Created with JetBrains PhpStorm.
- * Author Antonio Fullone : http://lastwebdesigner.com
+ * Author Antonio Fullone : http://antoniofullone.com
  * Date: 3/18/13
  * Time: 19:46 PM
  * Simple casperjs script to grab screenshots of different web pages in various viewports.
@@ -18,7 +17,6 @@
             {w:1440,h:960},
             {w:1920,h:1080}
         ],
-
         init: function() {
             this.casper = require('casper').create({
                 logLevel : "warning",
@@ -32,37 +30,42 @@
             var fs = require('fs');
             var data = fs.read('urls.txt');
             this.takeScreenshots(data);
-
-        },   
+        },
 
         takeScreenshots: function(data) {
             var urls = data;
+            var http = require('http');
             var lines = urls.split('\n');
             var folderPath ="Screenshots/";
             var casper = this.casper;
             var viewports = this.viewports;
+            if (lines.length > 0) {
+                casper.start().each(lines,function(self, link){
+                    if(link == ''){ 
+                        // Do nothing and skip the link
+                    } else{
+                        self.thenOpen(link, function(){
+                            viewports.forEach(
+                                function(doc){
+                                    casper.echo(name);
+                                    casper.viewport(doc.w,doc.h);
+                                    var title = casper.getTitle();
+                                    title = title.replace(/ /g,'_');
+                                    title = title.toLowerCase();
+                                    casper.echo("Opening : " + casper.getCurrentUrl() + "resolution :" + doc.w);
+                                    casper.capture(folderPath + title + "_" + doc.w + ".png");
+                                }
+                            )
+                        })
+                    }
+                });
+            } else {
+                casper.die('Error, add at least on link to start the responsive test');
+            }
 
-            casper.start().each(lines,function(self,link){
-                if(link == ''){
-                casper.die("add the url's to the file urls.txt");
-                } else{
-                    self.thenOpen(link,function(){
-                        viewports.forEach(
-                        function(doc){
-                            casper.echo(name);
-                            casper.viewport(doc.w,doc.h);
-                            var title = casper.getTitle();
-                            title = title.replace(/ /g,'_');
-                            title = title.toLowerCase();
-                            casper.echo("Opening : " + casper.getCurrentUrl() + "resolution :" + doc.w);
-                            casper.capture(folderPath + title + "_" + doc.w + ".png");
-                            }
-                        )
-                    })
-                }
-            });
             casper.run();
-        }     
+        }
     };
     kaidan.init();
 })();
+
